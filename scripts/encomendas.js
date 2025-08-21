12991c175e5e44e43957a1719446b0cb07df019f
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2025-08-20 22:31:25 -0300

    feacture
    melhorando e economizando o código

--- scripts/encomendas.js
+++ scripts/encomendas.js
@@ -6,21 +6,26 @@ for (let count=0; count < clientes.length; count++) {
     // seleciona a quantidade e valor unitario de cada linha
     let qtd = parseInt(clientes[count].querySelector(".qtd").textContent);
     let valorUn = parseInt(clientes[count].querySelector(".valorUn").textContent);
+    let produtoValido = true;
     
     // verifica o valor de "qtd", se for inválido informa na tabela, se não calcula o total
     if(qtd < 1 || isNaN(qtd)) {
         clientes[count].querySelector(".qtd").style.color = "red";
         clientes[count].querySelector(".qtd").textContent = "Quantidade inválida!";
 
-        
-    } else {
-        clientes[count].querySelector(".total").textContent = calculoTotal(qtd, valorUn);
-    }
-
-    
-    if(valorUn < 1 || isNaN(valorUn)) {
+        produtoValido = false;
+    } else if(valorUn < 1 || isNaN(valorUn)) {
         clientes[count].querySelector(".valorUn").style.color = "red";
         clientes[count].querySelector(".valorUn").textContent = "Valor inválido!";
+
+        produtoValido = false;
+    }
+
+    if (produtoValido) {
+        clientes[count].querySelector(".total").textContent = calculoTotal(qtd, valorUn)
+    } else {
+        clientes[count].querySelector(".total").textContent = 0;
+        clientes[count].querySelector(".total").style.color = "red";
     }
 }
 
